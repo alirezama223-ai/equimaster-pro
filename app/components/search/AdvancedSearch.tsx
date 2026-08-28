4671e526981f58953ea6cb33ac5ddae7f78b1cbe
alirezama223-ai <alirezama223@gmail.com>
2026-08-28 14:06:15 +0200

    refactor: unify search localization

--- app/components/search/AdvancedSearch.tsx
+++ app/components/search/AdvancedSearch.tsx
@@ -5,32 +5,10 @@ import SearchableSelect from "@/app/components/shared/SearchableSelect";
 import { getBreedSelectOptions } from "@/app/lib/breeds";
 import { COUNTRIES } from "@/app/lib/constants/countries";
 import { DISCIPLINES } from "@/app/lib/constants/disciplines";
+import { localizeSearchOptionLabel } from "@/app/lib/i18n/localizeSearchOption";
 import { SortOption } from "@/app/lib/horse-filters";
 
 const breedOptions = getBreedSelectOptions();
-
-const disciplineTranslations: Record<string, Record<string, string>> = {
-  de: {
-    barrel_racing: "Barrel Racing", breakaway_roping: "Breakaway Roping", breeding_stallion: "Zuchthengst", broodmare: "Zuchtstute", circus: "Zirkus", combined_driving: "Vielseitigkeitsfahren", cutting: "Cutting", dressage: "Dressur", driving: "Fahren", endurance: "Distanzreiten", equitation: "Reitkunst", eventing: "Vielseitigkeit", flat_racing: "Flachrennen", gaited_horses: "Gangpferde", harness_racing: "Trabrennen", horseball: "Horseball", hunter: "Hunter", hunter_jumper: "Hunter-Jumper", icelandic: "Islandpferd", leisure: "Freizeitreiten", military_horse: "Militärpferd", mounted_archery: "Mounted Archery", mounted_games: "Mounted Games", other: "Sonstiges", pleasure_riding: "Freizeitreiten", pole_bending: "Pole Bending", police_horse: "Polizeipferd", polo: "Polo", ranch_riding: "Ranch Riding", ranch_trail: "Ranch Trail", reining: "Reining", roping: "Roping", show_jumping: "Springreiten", steeplechase: "Hindernisrennen", team_penning: "Team Penning", therapy_horse: "Therapiepferd", trec: "TREC", trail: "Trail", vaulting: "Voltigieren", western_pleasure: "Western Pleasure", working_equitation: "Working Equitation", young_horse: "Jungpferd"
-  },
-  fr: {
-    barrel_racing: "Barrel Racing", breakaway_roping: "Breakaway Roping", breeding_stallion: "Étalon reproducteur", broodmare: "Jument poulinière", circus: "Cirque", combined_driving: "Attelage combiné", cutting: "Cutting", dressage: "Dressage", driving: "Attelage", endurance: "Endurance", equitation: "Équitation", eventing: "Concours complet", flat_racing: "Courses de plat", gaited_horses: "Chevaux à allures", harness_racing: "Trot attelé", horseball: "Horseball", hunter: "Hunter", hunter_jumper: "Hunter-Jumper", icelandic: "Islandais", leisure: "Loisir", military_horse: "Cheval militaire", mounted_archery: "Tir à l'arc à cheval", mounted_games: "Jeux équestres", other: "Autre", pleasure_riding: "Équitation de loisir", pole_bending: "Pole Bending", police_horse: "Cheval de police", polo: "Polo", ranch_riding: "Ranch Riding", ranch_trail: "Ranch Trail", reining: "Reining", roping: "Roping", show_jumping: "Saut d'obstacles", steeplechase: "Steeple-chase", team_penning: "Team Penning", therapy_horse: "Cheval de thérapie", trec: "TREC", trail: "Trail", vaulting: "Voltige", western_pleasure: "Western Pleasure", working_equitation: "Équitation de travail", young_horse: "Jeune cheval"
-  },
-  nl: {
-    barrel_racing: "Barrel Racing", breakaway_roping: "Breakaway Roping", breeding_stallion: "Fokhengst", broodmare: "Fokmerrie", circus: "Circus", combined_driving: "Gecombineerd mennen", cutting: "Cutting", dressage: "Dressuur", driving: "Mennen", endurance: "Endurance", equitation: "Rijkunst", eventing: "Eventing", flat_racing: "Vlakke rennen", gaited_horses: "Gangpaarden", harness_racing: "Draverij", horseball: "Horseball", hunter: "Hunter", hunter_jumper: "Hunter-Jumper", icelandic: "IJslander", leisure: "Recreatief rijden", military_horse: "Militair paard", mounted_archery: "Boogschieten te paard", mounted_games: "Ruiterspelen", other: "Overig", pleasure_riding: "Recreatief paardrijden", pole_bending: "Pole Bending", police_horse: "Politiepaard", polo: "Polo", ranch_riding: "Ranch Riding", ranch_trail: "Ranch Trail", reining: "Reining", roping: "Roping", show_jumping: "Springen", steeplechase: "Steeplechase", team_penning: "Team Penning", therapy_horse: "Therapiepaard", trec: "TREC", trail: "Trail", vaulting: "Voltige", western_pleasure: "Western Pleasure", working_equitation: "Working Equitation", young_horse: "Jong paard"
-  },
-  es: {
-    barrel_racing: "Carreras de barriles", breakaway_roping: "Breakaway Roping", breeding_stallion: "Semental reproductor", broodmare: "Yegua de cría", circus: "Circo", combined_driving: "Enganche combinado", cutting: "Cutting", dressage: "Doma clásica", driving: "Enganche", endurance: "Raid", equitation: "Equitación", eventing: "Concurso completo", flat_racing: "Carreras lisas", gaited_horses: "Caballos de aires", harness_racing: "Trote con arnés", horseball: "Horseball", hunter: "Hunter", hunter_jumper: "Hunter-Jumper", icelandic: "Caballo islandés", leisure: "Ocio", military_horse: "Caballo militar", mounted_archery: "Tiro con arco a caballo", mounted_games: "Juegos ecuestres", other: "Otros", pleasure_riding: "Equitación de ocio", pole_bending: "Pole Bending", police_horse: "Caballo policía", polo: "Polo", ranch_riding: "Ranch Riding", ranch_trail: "Ranch Trail", reining: "Reining", roping: "Roping", show_jumping: "Salto de obstáculos", steeplechase: "Steeplechase", team_penning: "Team Penning", therapy_horse: "Caballo de terapia", trec: "TREC", trail: "Trail", vaulting: "Volteo", western_pleasure: "Western Pleasure", working_equitation: "Equitación de trabajo", young_horse: "Caballo joven"
-  }
-};
-
-function getCountryLabel(alpha2: string, locale: string, fallback: string): string {
-  try {
-    return new Intl.DisplayNames([locale], { type: "region" }).of(alpha2) ?? fallback;
-  } catch {
-    return fallback;
-  }
-}
 
 type Props = {
   search: string;
@@ -100,16 +78,22 @@ export default function AdvancedSearch({
 }: Props) {
   const t = useTranslations("home");
   const locale = useLocale();
-  const countryOptions = COUNTRIES.map((countryOption) => ({
-    value: countryOption.name,
-    label: getCountryLabel(countryOption.alpha2, locale, countryOption.name),
-    searchText: [countryOption.name, getCountryLabel(countryOption.alpha2, locale, countryOption.name)].join(" "),
-  }));
-  const disciplineOptions = DISCIPLINES.map((discipline) => ({
-    value: discipline.label,
-    label: disciplineTranslations[locale]?.[discipline.id] ?? discipline.label,
-    searchText: [discipline.label, disciplineTranslations[locale]?.[discipline.id] ?? discipline.label].join(" "),
-  }));
+  const countryOptions = COUNTRIES.map((countryOption) => {
+    const label = localizeSearchOptionLabel(countryOption.name, countryOption.name, locale);
+    return {
+      value: countryOption.name,
+      label,
+      searchText: [countryOption.name, label].join(" "),
+    };
+  });
+  const disciplineOptions = DISCIPLINES.map((discipline) => {
+    const label = localizeSearchOptionLabel(discipline.label, discipline.label, locale);
+    return {
+      value: discipline.label,
+      label,
+      searchText: [discipline.label, label].join(" "),
+    };
+  });
 
   return (
     <section className="bg-[#08111F] py-14">
