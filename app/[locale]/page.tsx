a262896ca5a87b69a70c0995c0ca918fb5a9afb6
alirezama223-ai <alirezama223@gmail.com>
2026-09-01 20:17:41 +0200

    feat: load active homepage advertisements

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,4 +1,5 @@
 import { getActiveHorseListings } from "@/app/actions/horse-listings";
+import { getActiveHomepageAdvertisements } from "@/app/actions/advertisements";
 import { getHeroStats } from "@/app/actions/home-stats";
 import { getUserFavoriteListingIds } from "@/app/actions/favorites";
 import HomeClient from "@/app/components/home/HomeClient";
@@ -54,10 +55,11 @@ export default async function Home() {
     ],
   };
 
-  const [{ data: listingRows }, favoriteListingIds, heroStats] = await Promise.all([
+  const [{ data: listingRows }, favoriteListingIds, heroStats, advertisements] = await Promise.all([
     getActiveHorseListings(100),
     getUserFavoriteListingIds(),
     getHeroStats(),
+    getActiveHomepageAdvertisements(),
   ]);
   const marketplaceHorses = listingRows.map((row) =>
     listingRowToHorse(row, { priceOnRequestLabel: tCommon("priceOnRequest") })
@@ -73,6 +75,7 @@ export default async function Home() {
         marketplaceHorses={marketplaceHorses}
         favoriteListingIds={favoriteListingIds}
         heroStats={heroStats}
+        advertisements={advertisements}
         premiumStallions={<PremiumStallions />}
       />
     </>
